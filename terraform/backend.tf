ebb34f8686264af4c11273f37c0049743610ec03
Nikhhel07 <nikhilnarine70@gmail.com>
2025-07-21 14:30:07 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-python1"
+    bucket         = "terraform-state"
     key            = "greeting-app/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
